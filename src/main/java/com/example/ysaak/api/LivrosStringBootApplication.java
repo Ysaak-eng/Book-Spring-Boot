5773c7dfe60bb30f90b4b3c7fc17174f35914495
Ysaak <ysaakescolaluno@gmail.com>
2024-09-22 20:50:33 -0300

    primeira versão do api book

---
+++ src/main/java/com/example/ysaak/api/LivrosStringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.ysaak.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrosStringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrosStringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ysaak/api/LivrosStringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ysaak.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrosStringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
